eb7eb58e9f71f7f6c3c939ffddfefb56cb2766b1
zhuph <44405008+zhuph@users.noreply.github.com>
2019-10-26 20:18:38 +0800

    Update DemoApplication.java

--- main/java/com/example/demo/DemoApplication.java
+++ main/java/com/example/demo/DemoApplication.java
@@ -15,8 +15,6 @@ public class DemoApplication {
 
     @RequestMapping("hello")
     public Object hello(String[] args) {
-        System.out.println("hello Mr.Tree");
-        System.out.println("hello Mr.Zhu");
         System.out.println("hello Hui");
         return "hello";
     }
